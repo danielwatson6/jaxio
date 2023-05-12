735ea2ed0c300fc0346cb538a1999a0cbe62c85d
Daniel Watson <watsondaniel6@gmail.com>
2023-05-12 11:16:01 -0400

    add license

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
   requirements = re.sub(r'\n+', '\n', f.read()).strip().split('\n')
 
 setup(
-  name='jaxio',
-  version='0.0.1',
-  description='Input pipelines for JAX, in JAX',
-  long_description='An attempt to do input pipelines purely relying on JAX, with support for jitting iterators.',
-  author='Daniel Watson',
-  packages=['jaxio'],
-  install_requires=requirements,
+    name='jaxio',
+    version='0.0.1',
+    description='Input pipelines for JAX, in JAX',
+    long_description='An attempt to do input pipelines purely relying on JAX, with support for jitting iterators.',
+    author='Daniel Watson',
+    packages=['jaxio'],
+    install_requires=requirements,
+    license_files=('LICENSE.txt',),
 )
